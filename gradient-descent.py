2e3cf9a65d93c9b82a1f972ad1b41e2ba7da61db
Franz Louis T. Cesista <franzlouiscesista@gmail.com>
2017-09-14 07:46:15 +0800

    used sympy for input of function

--- gradient-descent.py
+++ gradient-descent.py
@@ -1,19 +1,35 @@
-def function(x):
-    return x ** 2
+from sympy import *
 
-def derivative_at(x):
-    return 2*x
+acceptable_delta = 0.000001
 
-x = 10
-learning_rate = 0.1
-acceptable_range = 0.0001
+if __name__ == '__main__':
+    x = Symbol('x')
+    func = sympify(input("f(x) = "), evaluate = False)
+    derv = simplify(diff(func, x))
+    print("f'(x) = ", derv)
+    print()
 
-# gradient descent helps us find minimas of a function
-print("x", "f(x)")
-print(x, function(x))
-for i in range(1, 10000):
-    x -= learning_rate * derivative_at(x)
-    print(x, function(x))
-    if function(x) < acceptable_range:
-        print("timesteps:", i)
-        break
+    x_val = float(input("initial x value: "))
+    learning_rate = float(input("learning rate: "))
+    print()
+
+    # gradient descent helps us find a minima
+    # of a function starting from an arbitrary
+    # initial x value
+    last_y = func.evalf(10, subs={x: x_val})
+    print("     x        f(x)")
+    print("%10.6f" % x_val, "%10.6f" % last_y)
+
+    for i in range(1, 10000):
+        x_val -= learning_rate * derv.evalf(10, subs={x : x_val})
+        y = func.evalf(10, subs={x: x_val})
+        print("%10.6f" % x_val, "%10.6f" % y)
+
+        if abs(last_y - y) < acceptable_delta:
+            print("a minima is found @ around (%.6f, %.6f)" % (x_val, y))
+            print("in", i, "timesteps")
+            break
+
+        last_y = y
+    
+    plot(func, (x, -10, 10))
