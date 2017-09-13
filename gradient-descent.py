d1447781b0ce100f0c8977f0d2cf08538c3a2d9d
Franz Louis T. Cesista <franzlouiscesista@gmail.com>
2017-09-14 07:50:34 +0800

    added TODOs

--- gradient-descent.py
+++ gradient-descent.py
@@ -21,6 +21,10 @@ if __name__ == '__main__':
     print("%10.6f" % x_val, "%10.6f" % last_y)
 
     for i in range(1, 10000):
+        # TODO: explain why this works
+        # (basically, we're just incrementally 
+        #  going down the tangent of a curve)
+        # (like, x := x - learning rate * slope of tangent)
         x_val -= learning_rate * derv.evalf(10, subs={x : x_val})
         y = func.evalf(10, subs={x: x_val})
         print("%10.6f" % x_val, "%10.6f" % y)
@@ -31,5 +35,6 @@ if __name__ == '__main__':
             break
 
         last_y = y
-    
+
+    # TODO improve this visualization
     plot(func, (x, -10, 10))
