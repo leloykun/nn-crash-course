9981176848a1066565a0563fc2e1d36d5b6a3cae
Franz Louis T. Cesista <franzlouiscesista@gmail.com>
2017-09-14 06:31:27 +0800

    fixed gradient descent

--- gradient-descent.py
+++ gradient-descent.py
@@ -1,14 +1,19 @@
+def function(x):
+    return x ** 2
+
 def derivative_at(x):
     return 2*x
 
 x = 10
-a = 0.1
-delta = 0.001
+learning_rate = 0.1
+acceptable_range = 0.0001
 
-print(x)
+# gradient descent helps us find minimas of a function
+print("x", "f(x)")
+print(x, function(x))
 for i in range(1, 10000):
-    x -= a * derivative_at(x)
-    print(x)
-    if x < delta:
+    x -= learning_rate * derivative_at(x)
+    print(x, function(x))
+    if function(x) < acceptable_range:
         print("timesteps:", i)
         break
